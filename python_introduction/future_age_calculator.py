ed0f0dc97577f8568152101f24519905fb5c94c5
TheOCJorsh <ogheghajoshua@gmail.com>
2025-11-06 23:43:15 +0100

    Added future age calculator script

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+# This is a script for future age calculation
+
+# Defined Variable
+user_age = int(input("How old are you? "))
+added_age = 27
+
+# Calculated field will be age = user_age + added age
+age = user_age + added_age
+
+print(f"In 2050, you will be {age} years old.")
