461fabfae55322e750b34ea9b3115aadc7ae16a4
TheOCJorsh <ogheghajoshua@gmail.com>
2025-11-06 22:50:18 +0100

    Added Simple Interest Calculator file

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# simple Interest Calculator
+principal = 1000  # Principal amount ($1000)
+rate = 0.05 # Annual Rate of interest (5%)
+time = 3  # Duration in years
+
+# Calculate simple interest I= P*R*T
+interest = principal * rate * time
+
+# Display the result
+print(f"The simple interest is {interest}")
