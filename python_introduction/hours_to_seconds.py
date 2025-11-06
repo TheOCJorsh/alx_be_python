8ba3df968a69f119184644f0c4325b3fac4eb1d6
TheOCJorsh <ogheghajoshua@gmail.com>
2025-11-06 23:16:19 +0100

    Adding Honurs to Seconds Script

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,9 @@
+# This is a script to convert hours to seconds
+
+# Defined Variable
+hours = 2
+seconds = hours * 3600
+
+# Calaculation for the conversion will be seconds * hours
+
+print(f"{hours} hour(s) is {seconds} seconds")
